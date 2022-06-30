83dc92cdbfd8ff1d445925dfb6d2518aed80a827
Gindulina Adele <gindullinaadele@gmail.com>
2022-06-30 17:16:16 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Link, Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Social from "./components/Social";
 
 function App() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading
+          ml="8" size="md" fontWeight='semibold' color="cyan.400">portfolio</Heading>
+
+        <Spacer></Spacer>
+        <IconButton ml={2} icon={<FaInstagram />} isRound='true' onClick={() =>
+                        window.open("https://www.instagram.com/blntnn/")}></IconButton>
+        <IconButton ml={2} icon={<FaGithub />} isRound='true' onClick={() =>
+           window.open("https://www.github.com/blntnn/")}></IconButton>
+        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import { Button } from '@chakra-ui/button';
+import { useColorMode } from '@chakra-ui/color-mode'
+import { Image } from '@chakra-ui/image';
+import { Stack, Circle, Flex, Box, Text } from '@chakra-ui/layout';
+import { useMediaQuery } from '@chakra-ui/media-query';
+import React from 'react'
+
+function Header() {
+
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+    return (
+        <Stack>
+            <Circle position="absolute" bg="blue.100" opacity="0.1"
+                w="300px" h="300px" alignSelf="flex-end" />
+            <Flex direction={isNotSmallerScreen ? "row" : "column"}
+                spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                alignSelf="flex-start">
+                <Box mt={isNotSmallerScreen ? "0" : "16"} align='flex-start'>
+                    <Text fontSize="5xl" fontWeight="semibold">Hi, I am</Text>
+                    <Text fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text' >Gindullina Adele</Text>
+                    <Text color={isDark ? "gray.200" : "gray.600"}>I'm beginner to web programming. At the moment I am studying React JS and Chakra UI library. In the future, I wish to become a Front-end developer.</Text>
+                    <Button mt={8} colorScheme="blue" onClick={() =>
+                        window.open("mailto:gindullinaadele?subject=Приглашение%20на%20работу&body=Номер%20для%20связи:")
+                    }>Hire Me</Button>
+
+                </Box>
+                <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src='https://avatars.githubusercontent.com/u/108070568?v=4' />
+            </Flex>
+
+        </Stack>
+    )
+}
+
+export default Header
